3ad0ef41ab8dad606077fb6c6a8c23c50773ce41
mikeberll <m.berlanda@studnet.tugraz.at>
2020-02-06 17:34:45 +0100

    New ninjabet

--- ninjaBet.sql
+++ ninjaBet.sql
@@ -1,48 +1,52 @@
-CREATE TABLE `users` (
-  `id_u` integer PRIMARY KEY,
-  `username` varchar(255),
-  `inserted_at` timestamp
-);
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Erstellungszeit: 06. Feb 2020 um 16:31
+-- Server-Version: 10.4.10-MariaDB
+-- PHP-Version: 7.3.12
 
-CREATE TABLE `accounts` (
-  `id_a` integer PRIMARY KEY,
-  `id_u` integer,
-  `id_ws` integer,
-  `inserted_at` timestamp,
-  `cash` double
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE `website` (
-  `id_ws` integer PRIMARY KEY,
-  `name` varchar(255)
-);
 
-CREATE TABLE `bank_account` (
-  `id_b` integer PRIMARY KEY,
-  `id_u` integer,
-  `cash` double
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE `deposit` (
-  `id_d` integer,
-  `id_a` integer,
-  `id_b` integer,
-  `cash` double
-);
+--
+-- Datenbank: `ninjabet`
+--
 
-CREATE TABLE `bet` (
-  `id_bet` integer,
-  `id_acc1` integer,
-  `id_acc2` integer,
-  `cash1` double,
-  `cash2` double,
-  `win1` double,
-  `win2` double,
-  `winner` boolean
-);
+-- --------------------------------------------------------
 
-ALTER TABLE `accounts` ADD FOREIGN KEY (`id_u`) REFERENCES `users` (`id_u`);
+--
+-- Tabellenstruktur für Tabelle `access_table`
+--
 
-ALTER TABLE `accounts` ADD FOREIGN KEY (`id_ws`) REFERENCES `website` (`id_ws`);
+DROP TABLE IF EXISTS `access_table`;
+CREATE TABLE IF NOT EXISTS `access_table` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `surname` text NOT NULL,
+  `username` text NOT NULL,
+  `password` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
-ALTER TABLE `bank_account` ADD FOREIGN KEY (`id_u`) REFERENCES `users` (`id_u`);
+--
+-- Daten für Tabelle `access_table`
+--
+
+INSERT INTO `access_table` (`id`, `name`, `surname`, `username`, `password`) VALUES
+(1, 'Michele', 'Berlanda', 'root1', 'mike'),
+(2, 'Giacomo', 'Pilotti', 'root2', 'jack');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
