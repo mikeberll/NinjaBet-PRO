4b7c573cc548e659d9488fd799027385638cb574
mikeberll <m.berlanda@studnet.tugraz.at>
2020-02-09 17:11:10 +0100

    PHP for transaction & modification of view (name and surname printed and not username and password

--- view.php
+++ view.php
@@ -1,6 +1,6 @@
 <?php
 require "conn.php";
-$mysql_qry = "select * from access_table";
+$mysql_qry = "select * from access_table;";
 $result = mysqli_query($conn, $mysql_qry);
 if (mysqli_num_rows($result) > 0) {
     
@@ -8,7 +8,7 @@ if (mysqli_num_rows($result) > 0) {
     // print JSON_encode($rows);
 
     while ($obj = $result->fetch_object()) {
-        echo $obj->username.' '.$obj->password."\n";
+        echo $obj->name.' '.$obj->surname."\n";
     }
 }
 else {
